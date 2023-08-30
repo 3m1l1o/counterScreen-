7d51b101d3a963059a64faa3deb80beb4acea979
unknown <emiliogallo80@gmail.com>
2023-08-30 13:38:18 -0300

    counterScreen-useReducer

---
+++ src/screens/TextScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+
+function TextScreen() {
+  return (
+    <View>
+        <Text>Text screen </Text>
+    </View>
+  )
+}
+ 
+const styles = StyleSheet.create({})
+
+export default TextScreen
